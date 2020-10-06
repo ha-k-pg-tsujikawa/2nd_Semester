fcc9d4fce189cd4dbf75d26ccd463115830a65c3
MSI\tsuji <pokebey63@gmail.com>
2020-10-06 13:47:45 +0900

    リファクタリング前、ステータスを画面に表示する処理を追加

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -20,7 +20,14 @@ public:
 
 Fighter::Fighter()
 {
-
+	m_hp = 0;
+	m_str = 0;
+	m_mag = 0;
+	m_tec = 0;
+	m_spd = 0;
+	m_def = 0;
+	m_luc = 0;
+	m_mde = 0;
 }
 
 Fighter::~Fighter()
@@ -30,6 +37,16 @@ Fighter::~Fighter()
 
 int main()
 {
+	Fighter fighter;
+
+	printf("hp  = %d\n", fighter.m_hp);
+	printf("str = %d\n", fighter.m_str);
+	printf("mag = %d\n", fighter.m_mag);
+	printf("tec = %d\n", fighter.m_tec);
+	printf("spd = %d\n", fighter.m_spd);
+	printf("def = %d\n", fighter.m_def);
+	printf("luc = %d\n", fighter.m_luc);
+	printf("mde = %d\n", fighter.m_mde);
 
 	system("pause");
 	return 0;
