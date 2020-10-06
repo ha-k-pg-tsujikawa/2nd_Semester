49528da2eb17df019f9194fc139cfabe0d719ac2
MSI\tsuji <pokebey63@gmail.com>
2020-10-06 14:27:15 +0900

    リファクタリングその1 ・クラス名を変更 ・クラスの定義を別のcpp、hに移動 ・よく使うヘッダーファイルのインクルードを、Common.hに集約

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -1,43 +1,9 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
-
-class Fighter
-{
-public:
-
-	Fighter();
-	~Fighter();
-
-	int m_hp;
-	int m_str;
-	int m_mag;
-	int m_tec;
-	int m_spd;
-	int m_def;
-	int m_luc;
-	int m_mde;
-};
-
-Fighter::Fighter()
-{
-	m_hp = 0;
-	m_str = 0;
-	m_mag = 0;
-	m_tec = 0;
-	m_spd = 0;
-	m_def = 0;
-	m_luc = 0;
-	m_mde = 0;
-}
-
-Fighter::~Fighter()
-{
-
-}
+﻿#include "Common.h"
+#include "Unit.h"
 
 int main()
 {
-	Fighter fighter;
+	Unit fighter;
 
 	printf("hp  = %d\n", fighter.m_hp);
 	printf("str = %d\n", fighter.m_str);
