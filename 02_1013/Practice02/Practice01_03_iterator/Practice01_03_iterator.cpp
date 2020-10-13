f388999652ae365c81c02cc97657ae227d9a3294
MSI\tsuji <pokebey63@gmail.com>
2020-10-13 14:20:44 +0900

    iteratorの使い方について学ぶ

---
+++ 02_1013/Practice02/Practice01_03_iterator/Practice01_03_iterator.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <time.h>
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    std::vector<int> v1(8); // 要素8、すべて0で初期化
+
+    // iteratorを使ったfor文の回し方
+    // for(int i = 0; i < v1.size(); i++) と同じ
+    int counter = 0;
+    for (std::vector<int>::iterator itr = v1.begin();itr != v1.end(); ++itr)
+    {
+        *itr = rand();
+        printf("v1[%d] = %d\n", counter, *itr);
+        counter++;
+    }
+    printf("\n");
+
+    system("pause");
+    return 0;
+}
+
+/*
+    std::vector<型>::iterator itr = v1.begin();
+                                          ↑
+                                要素の先頭を指すイテレーター
+    end()はポインタで言う nullptr
+    ++itr なのはそのほうが高速に動作するから
+    iteratorはポインタ
+    中の数字をいじりたいときは*を付ける必要がある
+*/
