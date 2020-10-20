7b923c173d283a22904f385f61426f000d05a981
MSI\tsuji <pokebey63@gmail.com>
2020-10-20 10:16:56 +0900

    当たり判定用に、幅と高さの変数、関数を追加

--- 03_1020/Practice03/Practice03_01/Base.cpp
+++ 03_1020/Practice03/Practice03_01/Base.cpp
@@ -6,11 +6,21 @@ Base::Base()
 	m_PosX = 0.0f;
 	m_PosY = 0.0f;
 	m_MoveSpeed = 1.0f;
+
+	m_Width = 10;
+	m_Height = 10;
 }
 
 Base::~Base()
 {
 
+}
+
+// ˆø”‚Ì‹éŒ`î•ñ‚ÆA©•ª‚ª“–‚½‚Á‚Ä‚¢‚é‚©”»’è
+bool Base::CheckHit(int x, int y, int width, int height)
+{
+	// ‰½‚©‚µ‚ç‚Ì“–‚½‚è”»’è
+	return false;
 }
 
 void Base::SetHp(int hp)
@@ -27,6 +37,12 @@ void Base::SetPos(float x, float y)
 void Base::SetMoveSpeed(float speed)
 {
 	m_MoveSpeed = speed;
+}
+
+void Base::SetSize(int width, int height)
+{
+	m_Width = width;
+	m_Height = height;
 }
 
 int Base::GetHp()
@@ -47,4 +63,14 @@ float Base::GetPosY()
 float Base::GetMoveSpeed()
 {
 	return m_MoveSpeed;
+}
+
+int Base::GetWidth()
+{
+	return m_Width;
+}
+
+int Base::GetHeight()
+{
+	return m_Height;
 }
