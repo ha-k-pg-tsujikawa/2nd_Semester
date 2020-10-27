7a681129e48f3013784ec2185e350df6b7637262
MSI\tsuji <pokebey63@gmail.com>
2020-10-27 10:39:51 +0900

    第4回の実習用プロジェクトを作成
    メンバイニシャライザについての実習
    
    イニシャライザの初期化順の注意点
    
    引数付きのコンストラクタのイニシャライザ
    派生クラスで、基底クラスのイニシャライザを使う

---
+++ 04_1027/Practice04/Practice04_01_Initializer/Practice04_01_Initializer.cpp
@@ -0,0 +1,83 @@
+﻿
+#include <iostream>
+
+class CharaBase
+{
+public:
+    CharaBase();
+    CharaBase(int hp, int mp);
+    ~CharaBase();
+
+    void PrintParam();
+
+private:
+    int Hp;
+    int Mp;
+
+    const int HP_MAX;
+};
+
+// コンストラクタで、メンバイニシャライザによる初期化のやり方
+// クラス名::コンストラクタ(引数) : メンバ変数(初期値), メンバ変数(初期値)
+// 注意点 : メンバイニシャライザは、変数の宣言順に呼び出される
+//          イニシャライザに書いた順番でない点に注意
+CharaBase::CharaBase()
+    : Hp(100)
+    , Mp(10)
+    , HP_MAX(99999999)  // constメンバ変数もここでなら初期化できる
+{
+    // 従来の方法
+    // Hp = 10;
+    // Mp = 10;
+    // HP_MAX = 999;    // コンストラクタ内ではconst型のメンバ変数を初期化できない
+}
+
+CharaBase::CharaBase(int hp, int mp)
+    : Hp(hp)
+    , Mp(mp)
+    , HP_MAX(999)
+{
+
+}
+
+CharaBase::~CharaBase()
+{
+
+}
+
+void CharaBase::PrintParam()
+{
+    printf("Hp = %d\n", Hp);
+    printf("Mp = %d\n", Mp);
+    printf("HP_MAX = %d\n", HP_MAX);
+}
+
+// CharaBaseを継承した派生クラスPlayer
+class Player : public CharaBase
+{
+public:
+    Player();
+
+private:
+    int Money;
+};
+
+Player::Player()
+    : CharaBase(200, 30)
+    , Money(50)
+{
+
+}
+
+int main()
+{
+    // データ型にもコンストラクタはある
+    int value(10);
+    float f_value(10.0f);
+
+    CharaBase ch;
+    ch.PrintParam();
+
+    Player player;
+    player.PrintParam();
+}
